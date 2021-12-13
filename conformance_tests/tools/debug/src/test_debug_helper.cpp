4178dbee17087bc5270f79ebb8a67b1d62a81faa
Matias Cabral <matias.a.cabral@intel.com>
2021-12-12 19:44:26 -0800

    Fix issues Module load events
    Implementation can have internal modules, so more events
    can be generated.
    
    Signed-off-by: Matias Cabral <matias.a.cabral@intel.com>

--- conformance_tests/tools/debug/src/test_debug_helper.cpp
+++ conformance_tests/tools/debug/src/test_debug_helper.cpp
@@ -206,8 +206,11 @@ void multiple_modules_created_test(ze_context_handle_t context,
   auto module = lzt::create_module(context, device, "debug_add.spv",
                                    ZE_MODULE_FORMAT_IL_SPIRV, "-g", nullptr);
 
+  auto kernel = lzt::create_function(module, "debug_add_constant_2");
+  //  auto module2 = lzt::create_module(context, device, "debug_add.spv",
   auto module2 = lzt::create_module(context, device, "1kernel.spv",
                                     ZE_MODULE_FORMAT_IL_SPIRV, "-g", nullptr);
+  // auto kernel2 = lzt::create_function(module2, "debug_add_constant_2");
   auto kernel2 = lzt::create_function(module2, "kernel1");
 
   ze_group_count_t group_count = {};
@@ -218,73 +221,55 @@ void multiple_modules_created_test(ze_context_handle_t context,
                               nullptr);
 
   auto size = 8192;
-  const int addval = 3;
+  auto buffer_a = lzt::allocate_shared_memory(size, 0, 0, 0, device, context);
+  auto buffer_b = lzt::allocate_device_memory(size, 0, 0, 0, device, context);
 
-  auto kernels = std::vector<ze_kernel_handle_t>(num_kernels);
-  auto initial_buffers = std::vector<void *>(num_kernels);
-  auto final_buffers = std::vector<void *>(num_kernels);
-  for (int k = 0; k < num_kernels; k++) {
-    auto kernel = lzt::create_function(module, "debug_add_constant_2");
-
-    initial_buffers[k] =
-        lzt::allocate_shared_memory(size, 0, 0, 0, device, context);
-    final_buffers[k] =
-        lzt::allocate_device_memory(size, 0, 0, 0, device, context);
-
-    std::memset(initial_buffers[k], 0, size);
-    for (size_t i = 0; i < size; i++) {
-      static_cast<uint8_t *>(initial_buffers[k])[i] = (i & 0xFF);
-    }
-
-    lzt::set_argument_value(kernel, 0, sizeof(final_buffers[k]),
-                            &(final_buffers[k]));
-    lzt::set_argument_value(kernel, 1, sizeof(addval), &addval);
-
-    uint32_t group_size_x = 1;
-    uint32_t group_size_y = 1;
-    uint32_t group_size_z = 1;
-    lzt::suggest_group_size(kernel, size, 1, 1, group_size_x, group_size_y,
-                            group_size_z);
-    lzt::set_group_size(kernel, group_size_x, 1, 1);
-    ze_group_count_t group_count = {};
-    group_count.groupCountX = size / group_size_x;
-    group_count.groupCountY = 1;
-    group_count.groupCountZ = 1;
-
-    lzt::append_memory_copy(command_list, final_buffers[k], initial_buffers[k],
-                            size);
-    lzt::append_barrier(command_list);
-    lzt::append_launch_function(command_list, kernel, &group_count, nullptr, 0,
-                                nullptr);
-    lzt::append_barrier(command_list);
-    lzt::append_memory_copy(command_list, initial_buffers[k], final_buffers[k],
-                            size);
+  std::memset(buffer_a, 0, size);
+  for (size_t i = 0; i < size; i++) {
+    static_cast<uint8_t *>(buffer_a)[i] = (i & 0xFF);
   }
 
+  const int addval = 3;
+  lzt::set_argument_value(kernel, 0, sizeof(buffer_b), &buffer_b);
+  lzt::set_argument_value(kernel, 1, sizeof(addval), &addval);
+
+  uint32_t group_size_x = 1;
+  uint32_t group_size_y = 1;
+  uint32_t group_size_z = 1;
+  lzt::suggest_group_size(kernel, size, 1, 1, group_size_x, group_size_y,
+                          group_size_z);
+  lzt::set_group_size(kernel, group_size_x, 1, 1);
+  group_count.groupCountX = size / group_size_x;
+  group_count.groupCountY = 1;
+  group_count.groupCountZ = 1;
+
+  lzt::append_memory_copy(command_list, buffer_b, buffer_a, size);
+  lzt::append_barrier(command_list);
+  lzt::append_launch_function(command_list, kernel, &group_count, nullptr, 0,
+                              nullptr);
+  lzt::append_barrier(command_list);
+  lzt::append_memory_copy(command_list, buffer_a, buffer_b, size);
   lzt::close_command_list(command_list);
   lzt::execute_command_lists(command_queue, 1, &command_list, nullptr);
   lzt::synchronize(command_queue, UINT64_MAX);
 
   // validation
-  for (int k = 0; k < num_kernels; k++) {
-    for (size_t i = 0; i < size; i++) {
-      EXPECT_EQ(static_cast<uint8_t *>(initial_buffers[k])[i],
-                static_cast<uint8_t>((i & 0xFF) + addval));
+  for (size_t i = 0; i < size; i++) {
+    EXPECT_EQ(static_cast<uint8_t *>(buffer_a)[i],
+              static_cast<uint8_t>((i & 0xFF) + addval));
 
-      if (::testing::Test::HasFailure()) {
-        exit(1);
-      }
+    if (::testing::Test::HasFailure()) {
+      exit(1);
     }
   }
 
   // cleanup
-  for (int k = 0; k < num_kernels; k++) {
-    lzt::free_memory(context, initial_buffers[k]);
-    lzt::free_memory(context, final_buffers[k]);
-
-    lzt::destroy_function(kernels[k]);
-  }
+  lzt::free_memory(context, buffer_a);
+  lzt::free_memory(context, buffer_b);
+  lzt::destroy_function(kernel);
+  lzt::destroy_function(kernel2);
   lzt::destroy_module(module);
+  lzt::destroy_module(module2);
   lzt::destroy_command_list(command_list);
   lzt::destroy_command_queue(command_queue);
 }
@@ -302,7 +287,7 @@ void attach_after_module_destroyed_test(
   auto module = lzt::create_module(context, device, "debug_add.spv",
                                    ZE_MODULE_FORMAT_IL_SPIRV, "-g", nullptr);
 
-  auto kernels = std::vector<ze_kernel_handle_t>(num_kernels);
+  //  auto kernels = std::vector<ze_kernel_handle_t>(num_kernels);
 
   auto kernel = lzt::create_function(module, "debug_add_constant_2");
 
