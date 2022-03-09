257598ec970604ff1b73bb603eeb527f613f0516
yoon0805 <young.jin.yoon@intel.com>
2022-03-08 20:36:30 -0800

    Add memory_overcommit for shared memory
    Signed-off-by: Young Jin Yoon <young.jin.yoon@intel.com>

--- conformance_tests/core/test_memory_overcommit/src/test_memory_overcommit.cpp
+++ conformance_tests/core/test_memory_overcommit/src/test_memory_overcommit.cpp
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright (C) 2019 Intel Corporation
+ * Copyright (C) 2019-2022 Intel Corporation
  *
  * SPDX-License-Identifier: MIT
  *
@@ -18,6 +18,8 @@ namespace lzt = level_zero_tests;
 #include <level_zero/ze_api.h>
 
 namespace {
+
+enum shared_memory_type { SHARED_LOCAL, SHARED_CROSS, SHARED_SYSTEM };
 
 class zeDriverMemoryOvercommitTests
     : public ::testing::Test,
@@ -284,6 +286,14 @@ protected:
         LOG_INFO << "totalSize "
                  << driver_info[i].device_memory_properties[j].totalSize;
       }
+      driver_info[i].device_memory_access_properties =
+          new ze_device_memory_access_properties_t[device_count];
+      for (uint32_t j = 0; j < device_count; j++) {
+        EXPECT_EQ(ZE_RESULT_SUCCESS,
+                  zeDeviceGetMemoryAccessProperties(
+                      driver_info[i].device_handles[j],
+                      &driver_info[i].device_memory_access_properties[j]));
+      }
     }
 
     DriverInfo_ = driver_info;
@@ -301,9 +311,216 @@ protected:
       delete[] DriverInfo_[i].device_properties;
       delete[] DriverInfo_[i].device_compute_properties;
       delete[] DriverInfo_[i].device_memory_properties;
+      delete[] DriverInfo_[i].device_memory_access_properties;
     }
 
     delete[] DriverInfo_;
+  }
+
+  void test_memory_overcommit(uint32_t driver_index,
+                              uint32_t device_in_driver_index,
+                              uint32_t memory_size_multiple,
+                              ze_memory_type_t memory_type,
+                              shared_memory_type shr_mem_type = SHARED_LOCAL) {
+
+    LOG_INFO << "TEST ARGUMENTS "
+             << "driver_index " << driver_index << " device_in_driver_index "
+             << device_in_driver_index << " memory_size_multiple "
+             << memory_size_multiple;
+
+    collect_drivers_info();
+
+    EXPECT_LT(driver_index, DriverInfoCount_);
+    EXPECT_LT(device_in_driver_index,
+              DriverInfo_[driver_index].number_device_handles);
+
+    DriverInfo_t *driver_info = &DriverInfo_[driver_index];
+
+    ze_driver_handle_t driver_handle = driver_info->driver_handle;
+    ze_context_handle_t context = lzt::create_context(driver_handle);
+
+    ze_device_handle_t device_handle =
+        driver_info->device_handles[device_in_driver_index];
+
+    uint32_t maxSharedLocalMemory =
+        driver_info->device_compute_properties[device_in_driver_index]
+            .maxSharedLocalMemory;
+    uint64_t totalSize =
+        driver_info->device_memory_properties[device_in_driver_index].totalSize;
+
+    size_t pattern_memory_size = memory_size_multiple * maxSharedLocalMemory;
+    size_t pattern_memory_count = pattern_memory_size >> 3; // array of uint64_t
+
+    uint64_t *gpu_pattern_buffer;
+    uint64_t *gpu_expected_output_buffer;
+    uint64_t *gpu_found_output_buffer;
+    ze_device_memory_access_properties_t &device_properties =
+        driver_info->device_memory_access_properties[device_in_driver_index];
+    if (memory_type == ZE_MEMORY_TYPE_DEVICE) {
+      auto memory_access_cap = device_properties.deviceAllocCapabilities;
+      if ((memory_access_cap & ZE_MEMORY_ACCESS_CAP_FLAG_RW) == 0) {
+        LOG_INFO << "WARNING: Unable to allocate device memory, skipping";
+        free_drivers_info();
+        return;
+      }
+      gpu_pattern_buffer = (uint64_t *)level_zero_tests::allocate_device_memory(
+          pattern_memory_size, 8, 0, use_this_ordinal_on_device_, device_handle,
+          context);
+      gpu_expected_output_buffer =
+          (uint64_t *)level_zero_tests::allocate_device_memory(
+              output_size_, 8, 0, use_this_ordinal_on_device_, device_handle,
+              context);
+      gpu_found_output_buffer =
+          (uint64_t *)level_zero_tests::allocate_device_memory(
+              output_size_, 8, 0, use_this_ordinal_on_device_, device_handle,
+              context);
+    } else if (memory_type == ZE_MEMORY_TYPE_SHARED) {
+      if (shr_mem_type == SHARED_LOCAL) {
+        auto memory_access_cap =
+            device_properties.sharedSingleDeviceAllocCapabilities;
+        if ((memory_access_cap & ZE_MEMORY_ACCESS_CAP_FLAG_RW) == 0) {
+          LOG_INFO << "WARNING: Unable to allocate shared single device "
+                      "memory, skipping";
+          free_drivers_info();
+          return;
+        }
+        gpu_pattern_buffer =
+            (uint64_t *)level_zero_tests::allocate_shared_memory(
+                pattern_memory_size, 8, 0, use_this_ordinal_on_device_,
+                device_handle, context);
+        gpu_expected_output_buffer =
+            (uint64_t *)level_zero_tests::allocate_shared_memory(
+                output_size_, 8, 0, use_this_ordinal_on_device_, device_handle,
+                context);
+        gpu_found_output_buffer =
+            (uint64_t *)level_zero_tests::allocate_shared_memory(
+                output_size_, 8, 0, use_this_ordinal_on_device_, device_handle,
+                context);
+      } else if (shr_mem_type == SHARED_SYSTEM) { // system allocation
+        auto memory_access_cap =
+            device_properties.sharedSystemAllocCapabilities;
+        if ((memory_access_cap & ZE_MEMORY_ACCESS_CAP_FLAG_RW) == 0) {
+          LOG_INFO
+              << "WARNING: Unable to allocate shared system memory, skipping";
+          free_drivers_info();
+          return;
+        }
+        gpu_pattern_buffer = new uint64_t[output_count_];
+        gpu_expected_output_buffer = new uint64_t[output_count_];
+        gpu_found_output_buffer = new uint64_t[output_count_];
+      } else if (shr_mem_type == SHARED_CROSS) {
+        auto memory_access_cap =
+            device_properties.sharedCrossDeviceAllocCapabilities;
+        if ((memory_access_cap & ZE_MEMORY_ACCESS_CAP_FLAG_RW) == 0) {
+          LOG_INFO
+              << "WARNING: Unable to allocate shared cross memory, skipping";
+          free_drivers_info();
+          return;
+        }
+        uint32_t next_device_index = device_in_driver_index;
+        // find the first available device to use the memory
+        for (auto i = 0u; i < driver_info->number_device_handles; i++) {
+          if (i != device_in_driver_index) {
+            LOG_INFO << "Selecting device " << i << "to test overcommit";
+            next_device_index = i;
+            break;
+          }
+        }
+        // if there is only one device available, skip the test
+        if (next_device_index == device_in_driver_index) {
+          LOG_INFO << "WARNING: Cannot find another device to launch kernel, "
+                      "skipping";
+          free_drivers_info();
+          return;
+        }
+        gpu_pattern_buffer =
+            (uint64_t *)level_zero_tests::allocate_shared_memory(
+                pattern_memory_size, 8, 0, use_this_ordinal_on_device_,
+                device_handle, context);
+        gpu_expected_output_buffer =
+            (uint64_t *)level_zero_tests::allocate_shared_memory(
+                output_size_, 8, 0, use_this_ordinal_on_device_, device_handle,
+                context);
+        gpu_found_output_buffer =
+            (uint64_t *)level_zero_tests::allocate_shared_memory(
+                output_size_, 8, 0, use_this_ordinal_on_device_, device_handle,
+                context);
+        // overwrite device_handle to call run_function()
+        device_handle = driver_info->device_handles[next_device_index];
+      } else {
+        LOG_INFO << "WARNING: Unknown memory type, skipping";
+        free_drivers_info();
+        return;
+      }
+    }
+
+    uint64_t *host_expected_output_buffer = new uint64_t[output_count_];
+    std::fill(host_expected_output_buffer,
+              host_expected_output_buffer + output_count_, 0);
+
+    uint64_t *host_found_output_buffer = new uint64_t[output_count_];
+    std::fill(host_found_output_buffer,
+              host_found_output_buffer + output_count_, 0);
+
+    uint16_t pattern_base = std::rand() % 0xFFFF;
+
+    LOG_INFO << "PREPARE TO RUN START";
+    LOG_INFO << "totalSize " << totalSize;
+    LOG_INFO << "maxSharedLocalMemory " << maxSharedLocalMemory;
+    LOG_INFO << "gpu_pattern_buffer " << gpu_pattern_buffer;
+    LOG_INFO << "pattern_memory_count " << pattern_memory_count;
+    LOG_INFO << "pattern_memory_size " << pattern_memory_size;
+    LOG_INFO << "pattern_base " << pattern_base;
+    LOG_INFO << "gpu_expected_output_buffer " << gpu_expected_output_buffer;
+    LOG_INFO << "host_expected_output_buffer " << host_expected_output_buffer;
+    LOG_INFO << "gpu_found_output_buffer " << gpu_found_output_buffer;
+    LOG_INFO << "host_found_output_buffer " << host_found_output_buffer;
+    LOG_INFO << "output count " << output_count_;
+    LOG_INFO << "output size " << output_size_;
+    LOG_INFO << "PREPARE TO RUN END";
+
+    LOG_INFO << "call create module";
+    ze_module_handle_t module_handle =
+        create_module(context, device_handle, "test_fill_device_memory.spv");
+
+    LOG_INFO << "call run_functions";
+    run_functions(device_handle, module_handle, gpu_pattern_buffer,
+                  pattern_memory_count, pattern_base,
+                  host_expected_output_buffer, gpu_expected_output_buffer,
+                  host_found_output_buffer, gpu_found_output_buffer,
+                  output_count_, context);
+
+    LOG_INFO << "call free memory";
+    if (memory_type == ZE_MEMORY_TYPE_SHARED && shr_mem_type == SHARED_SYSTEM) {
+      delete[] gpu_pattern_buffer;
+      delete[] gpu_expected_output_buffer;
+      delete[] gpu_found_output_buffer;
+    } else {
+      level_zero_tests::free_memory(context, gpu_pattern_buffer);
+      level_zero_tests::free_memory(context, gpu_expected_output_buffer);
+      level_zero_tests::free_memory(context, gpu_found_output_buffer);
+    }
+    level_zero_tests::destroy_context(context);
+
+    LOG_INFO << "call destroy module";
+    EXPECT_EQ(ZE_RESULT_SUCCESS, zeModuleDestroy(module_handle));
+
+    LOG_INFO << "check output buffer";
+    bool memory_test_failure = false;
+    for (uint32_t i = 0; i < output_count_; i++) {
+      if (host_expected_output_buffer[i] || host_found_output_buffer[i]) {
+        LOG_INFO << "Index of difference " << i << " found "
+                 << host_found_output_buffer[i] << " expected "
+                 << host_expected_output_buffer[i];
+        memory_test_failure = true;
+      }
+    }
+
+    EXPECT_EQ(false, memory_test_failure);
+
+    free_drivers_info();
+    delete[] host_expected_output_buffer;
+    delete[] host_found_output_buffer;
   }
 
   typedef struct DriverInfo {
@@ -367,109 +584,65 @@ TEST_P(
   uint32_t device_in_driver_index = test_arguments.device_in_driver_index;
   uint32_t memory_size_multiple = test_arguments.memory_size_multiple;
 
-  LOG_INFO << "TEST ARGUMENTS "
-           << "driver_index " << driver_index << " device_in_driver_index "
-           << device_in_driver_index << " memory_size_multiple "
-           << memory_size_multiple;
-
-  collect_drivers_info();
-
-  EXPECT_LT(driver_index, DriverInfoCount_);
-  EXPECT_LT(device_in_driver_index,
-            DriverInfo_[driver_index].number_device_handles);
-
-  DriverInfo_t *driver_info = &DriverInfo_[driver_index];
-
-  ze_driver_handle_t driver_handle = driver_info->driver_handle;
-  ze_context_handle_t context = lzt::create_context(driver_handle);
-
-  ze_device_handle_t device_handle =
-      driver_info->device_handles[device_in_driver_index];
-
-  uint32_t maxSharedLocalMemory =
-      driver_info->device_compute_properties[0].maxSharedLocalMemory;
-  uint64_t totalSize = driver_info->device_memory_properties[0].totalSize;
-
-  size_t pattern_memory_size = memory_size_multiple * maxSharedLocalMemory;
-  size_t pattern_memory_count = pattern_memory_size >> 3; // array of uint64_t
-
-  uint64_t *gpu_pattern_buffer;
-  gpu_pattern_buffer = (uint64_t *)level_zero_tests::allocate_device_memory(
-      pattern_memory_size, 8, 0, use_this_ordinal_on_device_, device_handle,
-      context);
-
-  uint64_t *gpu_expected_output_buffer;
-  gpu_expected_output_buffer =
-      (uint64_t *)level_zero_tests::allocate_device_memory(
-          output_size_, 8, 0, use_this_ordinal_on_device_, device_handle,
-          context);
-  uint64_t *host_expected_output_buffer = new uint64_t[output_count_];
-  std::fill(host_expected_output_buffer,
-            host_expected_output_buffer + output_count_, 0);
-
-  uint64_t *gpu_found_output_buffer;
-  gpu_found_output_buffer =
-      (uint64_t *)level_zero_tests::allocate_device_memory(
-          output_size_, 8, 0, use_this_ordinal_on_device_, device_handle,
-          context);
-  uint64_t *host_found_output_buffer = new uint64_t[output_size_];
-  std::fill(host_found_output_buffer, host_found_output_buffer + output_count_,
-            0);
-
-  uint16_t pattern_base = std::rand() % 0xFFFF;
-
-  LOG_INFO << "PREPARE TO RUN START";
-  LOG_INFO << "totalSize " << totalSize;
-  LOG_INFO << "maxSharedLocalMemory " << maxSharedLocalMemory;
-  LOG_INFO << "gpu_pattern_buffer " << gpu_pattern_buffer;
-  LOG_INFO << "pattern_memory_count " << pattern_memory_count;
-  LOG_INFO << "pattern_memory_size " << pattern_memory_size;
-  LOG_INFO << "pattern_base " << pattern_base;
-  LOG_INFO << "gpu_expected_output_buffer " << gpu_expected_output_buffer;
-  LOG_INFO << "host_expected_output_buffer " << host_expected_output_buffer;
-  LOG_INFO << "gpu_found_output_buffer " << gpu_found_output_buffer;
-  LOG_INFO << "host_found_output_buffer " << host_found_output_buffer;
-  LOG_INFO << "output count " << output_count_;
-  LOG_INFO << "output size " << output_size_;
-  LOG_INFO << "PREPARE TO RUN END";
-
-  LOG_INFO << "call create module";
-  ze_module_handle_t module_handle =
-      create_module(context, device_handle, "test_fill_device_memory.spv");
-
-  LOG_INFO << "call run_functions";
-  run_functions(device_handle, module_handle, gpu_pattern_buffer,
-                pattern_memory_count, pattern_base, host_expected_output_buffer,
-                gpu_expected_output_buffer, host_found_output_buffer,
-                gpu_found_output_buffer, output_count_, context);
-
-  LOG_INFO << "call free memory";
-  level_zero_tests::free_memory(context, gpu_pattern_buffer);
-  level_zero_tests::free_memory(context, gpu_expected_output_buffer);
-  level_zero_tests::free_memory(context, gpu_found_output_buffer);
-  level_zero_tests::destroy_context(context);
-
-  LOG_INFO << "call destroy module";
-  EXPECT_EQ(ZE_RESULT_SUCCESS, zeModuleDestroy(module_handle));
-
-  LOG_INFO << "check output buffer";
-  bool memory_test_failure = false;
-  for (uint32_t i = 0; i < output_count_; i++) {
-    if (host_expected_output_buffer[i] || host_found_output_buffer[i]) {
-      LOG_INFO << "Index of difference " << i << " found "
-               << host_found_output_buffer[i] << " expected "
-               << host_expected_output_buffer[i];
-      memory_test_failure = true;
-    }
-  }
-
-  EXPECT_EQ(false, memory_test_failure);
-
-  free_drivers_info();
-  delete[] host_expected_output_buffer;
-  delete[] host_found_output_buffer;
+  test_memory_overcommit(driver_index, device_in_driver_index,
+                         memory_size_multiple, ZE_MEMORY_TYPE_DEVICE);
 }
 
+TEST_P(
+    zeDriverMemoryOvercommitTests,
+    GivenSharedMemoryWhenAllocationSizeLargerThanDeviceMaxMemoryThenMemoryIsPagedOffAndOnTheDevice) {
+
+  MemoryTestArguments_t test_arguments = {
+      std::get<0>(GetParam()), // driver index
+      std::get<1>(GetParam()), // device index within driver
+      std::get<2>(GetParam())  // memory size multiple, rounded up to uint16_t
+  };
+
+  uint32_t driver_index = test_arguments.driver_index;
+  uint32_t device_in_driver_index = test_arguments.device_in_driver_index;
+  uint32_t memory_size_multiple = test_arguments.memory_size_multiple;
+
+  test_memory_overcommit(driver_index, device_in_driver_index,
+                         memory_size_multiple, ZE_MEMORY_TYPE_SHARED);
+}
+
+TEST_P(
+    zeDriverMemoryOvercommitTests,
+    GivenSharedSystemMemoryWhenAllocationSizeLargerThanDeviceMaxMemoryThenMemoryIsPagedOffAndOnTheDevice) {
+
+  MemoryTestArguments_t test_arguments = {
+      std::get<0>(GetParam()), // driver index
+      std::get<1>(GetParam()), // device index within driver
+      std::get<2>(GetParam())  // memory size multiple, rounded up to uint16_t
+  };
+
+  uint32_t driver_index = test_arguments.driver_index;
+  uint32_t device_in_driver_index = test_arguments.device_in_driver_index;
+  uint32_t memory_size_multiple = test_arguments.memory_size_multiple;
+
+  test_memory_overcommit(driver_index, device_in_driver_index,
+                         memory_size_multiple, ZE_MEMORY_TYPE_SHARED,
+                         SHARED_SYSTEM);
+}
+
+TEST_P(
+    zeDriverMemoryOvercommitTests,
+    GivenSharedCrossMemoryWhenAllocationSizeLargerThanDeviceMaxMemoryThenMemoryIsPagedOffAndOnTheDevice) {
+
+  MemoryTestArguments_t test_arguments = {
+      std::get<0>(GetParam()), // driver index
+      std::get<1>(GetParam()), // device index within driver
+      std::get<2>(GetParam())  // memory size multiple, rounded up to uint16_t
+  };
+
+  uint32_t driver_index = test_arguments.driver_index;
+  uint32_t device_in_driver_index = test_arguments.device_in_driver_index;
+  uint32_t memory_size_multiple = test_arguments.memory_size_multiple;
+
+  test_memory_overcommit(driver_index, device_in_driver_index,
+                         memory_size_multiple, ZE_MEMORY_TYPE_SHARED,
+                         SHARED_CROSS);
+}
 INSTANTIATE_TEST_CASE_P(TestAllInputPermuntations,
                         zeDriverMemoryOvercommitTests,
                         ::testing::Combine(::testing::Values(0),
