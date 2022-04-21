93ac5bff51e7a6289f7dc13572dfd26e82f0a114
Kacper Kasper <kacper@kacperkasper.pl>
2022-04-21 14:49:56 +0200

    Add negative tests for device lost error

---
+++ negative_tests/core/test_cmdqueue_errors/kernels/gpu_hang.cl
@@ -0,0 +1,11 @@
+/*
+ *
+ * Copyright (C) 2022 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+kernel void hang() {
+    while(1);
+}
--- negative_tests/core/test_cmdqueue_errors/src/test_cmdqueue_errors.cpp
+++ negative_tests/core/test_cmdqueue_errors/src/test_cmdqueue_errors.cpp
@@ -165,4 +165,116 @@ TEST_F(
   lzt::destroy_command_queue(command_queue);
 }
 
+TEST_F(
+    CommandQueueExecuteCommandListNegativeTests,
+    GivenLongRunningKernelWhenExecutingCommandQueueSynchronizeThenDeviceLostErrorIsReturned) {
+  const ze_device_handle_t device = lzt::zeDevice::get_instance()->get_device();
+  ze_command_list_handle_t command_list;
+  ze_command_queue_handle_t command_queue = nullptr;
+  command_queue = lzt::create_command_queue(device);
+  command_list = lzt::create_command_list(device);
+
+  auto module = lzt::create_module(device, "gpu_hang.spv");
+  auto kernel = lzt::create_function(module, "hang");
+
+  lzt::set_group_size(kernel, 1, 1, 1);
+
+  ze_group_count_t group_count;
+  group_count.groupCountX = 1;
+  group_count.groupCountY = 1;
+  group_count.groupCountZ = 1;
+
+  lzt::append_launch_function(command_list, kernel, &group_count, nullptr, 0,
+                              nullptr);
+
+  lzt::close_command_list(command_list);
+  lzt::execute_command_lists(command_queue, 1, &command_list, nullptr);
+
+  EXPECT_EQ(ZE_RESULT_ERROR_DEVICE_LOST,
+            zeCommandQueueSynchronize(command_queue, UINT64_MAX));
+
+  lzt::destroy_function(kernel);
+  lzt::destroy_module(module);
+  lzt::destroy_command_list(command_list);
+  lzt::destroy_command_queue(command_queue);
+}
+
+TEST_F(
+    CommandQueueExecuteCommandListNegativeTests,
+    GivenLongRunningKernelWhenExecutingEventHostSynchronizeThenDeviceLostErrorIsReturned) {
+  const ze_device_handle_t device = lzt::zeDevice::get_instance()->get_device();
+  ze_command_list_handle_t command_list;
+  ze_command_queue_handle_t command_queue = nullptr;
+  command_queue = lzt::create_command_queue(device);
+  command_list = lzt::create_command_list(device);
+
+  ze_event_handle_t hEvent = nullptr;
+
+  lzt::zeEventPool ep;
+  ep.create_event(hEvent);
+
+  auto module = lzt::create_module(device, "gpu_hang.spv");
+  auto kernel = lzt::create_function(module, "hang");
+
+  lzt::set_group_size(kernel, 1, 1, 1);
+
+  ze_group_count_t group_count;
+  group_count.groupCountX = 1;
+  group_count.groupCountY = 1;
+  group_count.groupCountZ = 1;
+
+  lzt::append_launch_function(command_list, kernel, &group_count, hEvent, 0,
+                              nullptr);
+
+  lzt::close_command_list(command_list);
+  lzt::execute_command_lists(command_queue, 1, &command_list, nullptr);
+
+  EXPECT_EQ(ZE_RESULT_ERROR_DEVICE_LOST,
+            zeEventHostSynchronize(hEvent, UINT64_MAX));
+
+  ep.destroy_event(hEvent);
+
+  lzt::destroy_function(kernel);
+  lzt::destroy_module(module);
+  lzt::destroy_command_list(command_list);
+  lzt::destroy_command_queue(command_queue);
+}
+
+TEST_F(
+    CommandQueueExecuteCommandListNegativeTests,
+    GivenLongRunningKernelWhenExecutingFenceHostSynchronizeThenDeviceLostErrorIsReturned) {
+  const ze_device_handle_t device = lzt::zeDevice::get_instance()->get_device();
+  ze_command_list_handle_t command_list;
+  ze_command_queue_handle_t command_queue = nullptr;
+  ze_fence_handle_t hFence = nullptr;
+  command_queue = lzt::create_command_queue(device);
+  command_list = lzt::create_command_list(device);
+  hFence = lzt::create_fence(command_queue);
+
+  auto module = lzt::create_module(device, "gpu_hang.spv");
+  auto kernel = lzt::create_function(module, "hang");
+
+  lzt::set_group_size(kernel, 1, 1, 1);
+
+  ze_group_count_t group_count;
+  group_count.groupCountX = 1;
+  group_count.groupCountY = 1;
+  group_count.groupCountZ = 1;
+
+  lzt::append_launch_function(command_list, kernel, &group_count, nullptr, 0,
+                              nullptr);
+
+  lzt::close_command_list(command_list);
+  lzt::execute_command_lists(command_queue, 1, &command_list, hFence);
+
+  EXPECT_EQ(ZE_RESULT_ERROR_DEVICE_LOST,
+            zeFenceHostSynchronize(hFence, UINT64_MAX));
+
+  lzt::destroy_function(kernel);
+  lzt::destroy_module(module);
+  lzt::destroy_fence(hFence);
+  lzt::destroy_command_list(command_list);
+  lzt::destroy_command_queue(command_queue);
+}
+
 } // namespace
