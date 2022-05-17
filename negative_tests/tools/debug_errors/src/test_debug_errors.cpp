7a3af5b22cb3fe451fd712ceeb92b023e5079cb0
Matias Cabral <matias.a.cabral@intel.com>
2022-05-17 12:33:11 -0700

    Remove negative detach test
    Signed-off-by: Matias Cabral <matias.a.cabral@intel.com>

--- negative_tests/tools/debug_errors/src/test_debug_errors.cpp
+++ negative_tests/tools/debug_errors/src/test_debug_errors.cpp
@@ -68,57 +68,13 @@ protected:
     bin_name = "test_debug_errors_helper";
   }
 };
-class zetDebugDetachErrorsTest : public zetDebugErrorsBaseSetup {
-protected:
-  void run_detach_error_test(std::vector<ze_device_handle_t> &devices,
-                             bool use_sub_devices);
-};
-
-void zetDebugDetachErrorsTest::run_detach_error_test(
-    std::vector<ze_device_handle_t> &devices, bool use_sub_devices) {
-
-  for (auto &device : devices) {
-    if (!is_debug_supported(device)) {
-      continue;
-    }
-
-    debugHelper = launch_process(BASIC, device, use_sub_devices);
-
-    zet_debug_config_t debug_config = {};
-    debug_config.pid = debugHelper.id();
-
-    debugSession = lzt::debug_attach(device, debug_config);
-
-    synchro->notify_attach();
-    lzt::debug_detach(debugSession);
-
-    EXPECT_NE(ZE_RESULT_SUCCESS, zetDebugDetach(debugSession));
-
-    debugHelper.wait();
-  }
-}
+class zetDebugDetachErrorsTest : public zetDebugErrorsBaseSetup {};
 
 TEST_F(
     zetDebugDetachErrorsTest,
     GivenUnConfiguredUnattachedDebuggerWhenCallingDetachThenNotSuccessReturned) {
   zet_debug_session_handle_t debug_session = {};
   EXPECT_NE(ZE_RESULT_SUCCESS, zetDebugDetach(debug_session));
-}
-
-TEST_F(
-    zetDebugDetachErrorsTest,
-    GivenDebuggerDetachedFromDeviceApplicationWhenCallingDetachAgainThenNotSuccessReturned) {
-  auto driver = lzt::get_default_driver();
-  auto devices = lzt::get_devices(driver);
-
-  run_detach_error_test(devices, false);
-}
-
-TEST_F(
-    zetDebugDetachErrorsTest,
-    GivenDebuggerDetachedFromSubDeviceApplicationWhenCallingDetachAgainThenNotSuccessReturned) {
-  auto all_sub_devices = lzt::get_all_sub_devices();
-  run_detach_error_test(all_sub_devices, true);
 }
 
 } // namespace
