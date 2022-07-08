1d26a9f841fb5efc9589f48ef6b9736c5b2142de
gdtratnack <geoffrey.tratnack@intel.com>
2022-07-08 16:24:16 -0400

    Update zetKernelGetProfileInfo CTS to test possible flags
    * Fix for test_pin following zetKernelGetProfileInfo implementation
    
    * Running linter on test_pin cts
    
    Signed-off-by: Tratnack, Geoffrey <geoffrey.tratnack@intel.com>

--- conformance_tests/tools/pin/src/test_pin.cpp
+++ conformance_tests/tools/pin/src/test_pin.cpp
@@ -22,31 +22,65 @@ namespace {
 TEST(
     PINTests,
     GivenModuleCreatedWithProfileFlagsWhenGettingKernelProfileInfoThenValidPropertiesAreReturned) {
+  std::vector<uint32_t> profileFlags = {
+      ZET_PROFILE_FLAG_REGISTER_REALLOCATION,
+      ZET_PROFILE_FLAG_FREE_REGISTER_INFO,
+      (ZET_PROFILE_FLAG_REGISTER_REALLOCATION |
+       ZET_PROFILE_FLAG_FREE_REGISTER_INFO)};
 
-  auto driver = lzt::get_default_driver();
-  auto context = lzt::create_context(driver);
-  auto device = lzt::get_default_device(driver);
+  for (auto flag : profileFlags) {
+    auto driver = lzt::get_default_driver();
+    auto context = lzt::create_context(driver);
+    auto device = lzt::get_default_device(driver);
 
-  std::stringstream build_string;
-  build_string << "-zet-profile-flags " << std::hex
-               << ZET_PROFILE_FLAG_FREE_REGISTER_INFO;
+    std::stringstream build_string;
+    build_string << "-zet-profile-flags " << std::hex << flag;
 
-  auto module = lzt::create_module(context, device, "profile_module.spv",
-                                   ZE_MODULE_FORMAT_IL_SPIRV,
-                                   build_string.str().c_str(), nullptr);
+    auto module = lzt::create_module(context, device, "profile_module.spv",
+                                     ZE_MODULE_FORMAT_IL_SPIRV,
+                                     build_string.str().c_str(), nullptr);
 
-  auto kernel = lzt::create_function(module, "blackscholes");
+    auto kernel = lzt::create_function(module, "blackscholes");
 
-  zet_profile_properties_t kernel_profile_properties = {};
-  ASSERT_EQ(ZE_RESULT_SUCCESS,
-            zetKernelGetProfileInfo(kernel, &kernel_profile_properties));
+    zet_profile_properties_t kernel_profile_properties = {};
+    ASSERT_EQ(ZE_RESULT_SUCCESS,
+              zetKernelGetProfileInfo(kernel, &kernel_profile_properties));
 
-  EXPECT_EQ(kernel_profile_properties.flags,
-            ZET_PROFILE_FLAG_FREE_REGISTER_INFO);
+    EXPECT_EQ(kernel_profile_properties.flags, flag);
 
-  lzt::destroy_function(kernel);
-  lzt::destroy_module(module);
-  lzt::destroy_context(context);
+    lzt::destroy_function(kernel);
+    lzt::destroy_module(module);
+    lzt::destroy_context(context);
+  }
+}
+
+TEST(
+    PINTests,
+    GivenModuleCreatedWithInvalidProfileFlagsWhenGettingKernelProfileInfoThenDefaultPropertiesAreReturned) {
+  std::vector<std::string> invalidBuildStrings = {"", "-zet-profile-flags",
+                                                  "-zet-profile-flags ",
+                                                  "-zet-profile-flags garbage"};
+  for (auto buildString : invalidBuildStrings) {
+    auto driver = lzt::get_default_driver();
+    auto context = lzt::create_context(driver);
+    auto device = lzt::get_default_device(driver);
+
+    auto module = lzt::create_module(context, device, "profile_module.spv",
+                                     ZE_MODULE_FORMAT_IL_SPIRV,
+                                     buildString.c_str(), nullptr);
+
+    auto kernel = lzt::create_function(module, "blackscholes");
+
+    zet_profile_properties_t kernel_profile_properties = {};
+    ASSERT_EQ(ZE_RESULT_SUCCESS,
+              zetKernelGetProfileInfo(kernel, &kernel_profile_properties));
+
+    EXPECT_EQ(kernel_profile_properties.flags, 0);
+
+    lzt::destroy_function(kernel);
+    lzt::destroy_module(module);
+    lzt::destroy_context(context);
+  }
 }
 
 } // namespace
