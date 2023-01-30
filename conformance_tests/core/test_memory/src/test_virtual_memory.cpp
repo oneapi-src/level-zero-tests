39825b2425866a04253bd41032a2e36b32db6b7c
Neil Spruit <neil.r.spruit@intel.com>
2023-01-29 21:30:58 -0800

    CTS with Mapped offsets into Virtual Reservation and Test Name fix
    Related-To: VLCLJ-1842
    
    Signed-off-by: Neil R Spruit <neil.r.spruit@intel.com>

--- conformance_tests/core/test_memory/src/test_virtual_memory.cpp
+++ conformance_tests/core/test_memory/src/test_virtual_memory.cpp
@@ -80,9 +80,8 @@ TEST_F(
   lzt::virtual_memory_free(context, reservedVirtualMemory, allocationSize);
 }
 
-TEST_F(
-    zeVirtualMemoryTests,
-    GivenPageAlignedSizeThenVirtualAndPhysicalMemoryReservedAndMappedSuccessfully) {
+TEST_F(zeVirtualMemoryTests,
+       GivenPageAlignedSizeThenVirtualAndPhysicalMemoryReservedSuccessfully) {
   lzt::query_page_size(context, device, allocationSize, &pageSize);
   allocationSize = lzt::create_page_aligned_size(allocationSize, pageSize);
   lzt::physical_memory_allocation(context, device, allocationSize,
@@ -151,6 +150,69 @@ TEST_F(
   lzt::virtual_memory_unmap(context, reservedVirtualMemory, allocationSize);
   lzt::physical_memory_destroy(context, reservedPhysicalMemory);
   lzt::virtual_memory_free(context, reservedVirtualMemory, allocationSize);
+  lzt::free_memory(memory);
+  lzt::destroy_command_list(cmdlist);
+  lzt::destroy_command_queue(cmdqueue);
+}
+
+TEST_F(
+    zeVirtualMemoryTests,
+    GivenMultiMappedReadWriteMemoryOnOneDeviceThenFillAndCopyWithMappedVirtualMemorySucceeds) {
+  ze_command_list_handle_t cmdlist = lzt::create_command_list(device);
+  ze_command_queue_handle_t cmdqueue = lzt::create_command_queue();
+
+  allocationSize = 2097152 / 2;
+  ze_physical_mem_handle_t reservedPhysicalMemorySecondHalf;
+  lzt::query_page_size(context, device, allocationSize, &pageSize);
+  allocationSize = lzt::create_page_aligned_size(allocationSize, pageSize);
+  lzt::physical_memory_allocation(context, device, allocationSize,
+                                  &reservedPhysicalMemory);
+  lzt::physical_memory_allocation(context, device, allocationSize,
+                                  &reservedPhysicalMemorySecondHalf);
+  lzt::virtual_memory_reservation(context, nullptr, allocationSize * 2,
+                                  &reservedVirtualMemory);
+  EXPECT_NE(nullptr, reservedVirtualMemory);
+
+  ASSERT_EQ(zeVirtualMemMap(context, reservedVirtualMemory, allocationSize,
+                            reservedPhysicalMemory, 0,
+                            ZE_MEMORY_ACCESS_ATTRIBUTE_READWRITE),
+            ZE_RESULT_SUCCESS);
+  uint64_t offsetAddr =
+      reinterpret_cast<uint64_t>(reservedVirtualMemory) + allocationSize;
+  ASSERT_EQ(zeVirtualMemMap(context, reinterpret_cast<void *>(offsetAddr),
+                            allocationSize, reservedPhysicalMemorySecondHalf, 0,
+                            ZE_MEMORY_ACCESS_ATTRIBUTE_READWRITE),
+            ZE_RESULT_SUCCESS);
+  int8_t pattern = 9;
+  void *memory = lzt::allocate_shared_memory(allocationSize * 2, pageSize);
+  lzt::append_memory_fill(cmdlist, reservedVirtualMemory, &pattern,
+                          sizeof(pattern), allocationSize, nullptr);
+  lzt::append_barrier(cmdlist, nullptr, 0, nullptr);
+  lzt::append_memory_fill(cmdlist, reinterpret_cast<void *>(offsetAddr),
+                          &pattern, sizeof(pattern), allocationSize, nullptr);
+  lzt::append_barrier(cmdlist, nullptr, 0, nullptr);
+  lzt::append_memory_copy(cmdlist, memory, reservedVirtualMemory,
+                          allocationSize, nullptr);
+  lzt::append_barrier(cmdlist, nullptr, 0, nullptr);
+  uint64_t offsetSharedAddr =
+      reinterpret_cast<uint64_t>(memory) + allocationSize;
+  lzt::append_memory_copy(cmdlist, reinterpret_cast<void *>(offsetSharedAddr),
+                          reinterpret_cast<void *>(offsetAddr), allocationSize,
+                          nullptr);
+  lzt::close_command_list(cmdlist);
+  lzt::execute_command_lists(cmdqueue, 1, &cmdlist, nullptr);
+  lzt::synchronize(cmdqueue, UINT64_MAX);
+  uint8_t *data = reinterpret_cast<uint8_t *>(memory);
+  for (int i = 0; i < allocationSize * 2; i++) {
+    ASSERT_EQ(data[i], pattern);
+  }
+
+  lzt::virtual_memory_unmap(context, reservedVirtualMemory, allocationSize);
+  lzt::virtual_memory_unmap(context, reinterpret_cast<void *>(offsetAddr),
+                            allocationSize);
+  lzt::physical_memory_destroy(context, reservedPhysicalMemory);
+  lzt::physical_memory_destroy(context, reservedPhysicalMemorySecondHalf);
+  lzt::virtual_memory_free(context, reservedVirtualMemory, allocationSize * 2);
   lzt::free_memory(memory);
   lzt::destroy_command_list(cmdlist);
   lzt::destroy_command_queue(cmdqueue);
