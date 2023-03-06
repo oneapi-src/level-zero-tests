0d5589d943f9ff4c8c16f4e0d6ff5a79c8f13426
Raiyan Latif <raiyan.latif@intel.com>
2023-03-06 05:16:43 -0800

    Add CTS for P2P remote Image copy from/to local device Buffer
    - Created two CTS tests to cover the cases where an image copy
      from/to memory buffer is done between an image on the remote
      device and a memory buffer on the local device.
    
    Related-To: LOCI-1186
    
    Signed-off-by: Raiyan Latif <raiyan.latif@intel.com>

--- conformance_tests/core/test_p2p/src/test_p2p_image_copy.cpp
+++ conformance_tests/core/test_p2p/src/test_p2p_image_copy.cpp
@@ -319,6 +319,115 @@ TEST_P(P2PImageCopyMemory,
   lzt::free_memory(target_mem);
 }
 
+TEST_P(
+    P2PImageCopyMemory,
+    GivenImageOnRemoteDeviceWhenCopiedToMemoryOnLocalDeviceThenResultIsCorrect) {
+  if (skip)
+    return;
+
+  ze_event_desc_t event_desc = {};
+  event_desc.stype = ZE_STRUCTURE_TYPE_EVENT_DESC;
+  event_desc.index = 0;
+  event_desc.signal = ZE_EVENT_SCOPE_FLAG_HOST;
+  event_desc.wait = ZE_EVENT_SCOPE_FLAG_HOST;
+  auto event = lzt::create_event(ep, event_desc);
+
+  void *target_mem;
+  size_t mem_size = img_height * img_width * sizeof(uint32_t);
+  if (GetParam() == ZE_MEMORY_TYPE_DEVICE) {
+    ze_device_mem_alloc_flags_t d_flags = 0;
+    ze_host_mem_alloc_flags_t h_flags = 0;
+    target_mem = lzt::allocate_device_memory(mem_size, 1, d_flags, dev1,
+                                             lzt::get_default_context());
+  } else {
+    ze_device_mem_alloc_flags_t d_flags = 0;
+    ze_host_mem_alloc_flags_t h_flags = 0;
+    target_mem =
+        lzt::allocate_shared_memory(mem_size, 1, d_flags, h_flags, dev1);
+  }
+
+  // Load image to dev0
+  lzt::append_image_copy_from_mem(command_list_dev0, img_dev0,
+                                  input_png.raw_data(), nullptr);
+  lzt::close_command_list(command_list_dev0);
+  lzt::execute_command_lists(command_q_dev0, 1, &command_list_dev0, nullptr);
+  lzt::synchronize(command_q_dev0, UINT64_MAX);
+
+  // on dev1, copy dev0 image to dev1 memory
+  lzt::append_image_copy_to_mem(command_list_dev1, target_mem, img_dev0, event);
+  lzt::append_wait_on_events(command_list_dev1, 1, &event);
+
+  // Copyback to host
+  lzt::append_memory_copy(command_list_dev1, output_png.raw_data(), target_mem,
+                          mem_size);
+  lzt::close_command_list(command_list_dev1);
+  lzt::execute_command_lists(command_q_dev1, 1, &command_list_dev1, nullptr);
+  lzt::synchronize(command_q_dev1, UINT64_MAX);
+
+  // compare results
+  EXPECT_EQ(input_png, output_png);
+
+  lzt::destroy_event(event);
+  lzt::free_memory(target_mem);
+}
+
+TEST_P(
+    P2PImageCopyMemory,
+    GivenImageOnRemoteDeviceWhenCopiedFromMemoryOnLocalDeviceThenResultIsCorrect) {
+  if (skip)
+    return;
+
+  ze_event_desc_t event_desc = {};
+  event_desc.stype = ZE_STRUCTURE_TYPE_EVENT_DESC;
+  event_desc.index = 0;
+  event_desc.signal = ZE_EVENT_SCOPE_FLAG_HOST;
+  event_desc.wait = ZE_EVENT_SCOPE_FLAG_HOST;
+  auto event1 = lzt::create_event(ep, event_desc);
+  event_desc.index = 1;
+  auto event2 = lzt::create_event(ep, event_desc);
+
+  void *target_mem;
+  size_t mem_size = img_height * img_width * sizeof(uint32_t);
+  if (GetParam() == ZE_MEMORY_TYPE_DEVICE) {
+    ze_device_mem_alloc_flags_t d_flags = 0;
+    ze_host_mem_alloc_flags_t h_flags = 0;
+    target_mem = lzt::allocate_device_memory(mem_size, 1, d_flags, dev1,
+                                             lzt::get_default_context());
+  } else {
+    ze_device_mem_alloc_flags_t d_flags = 0;
+    ze_host_mem_alloc_flags_t h_flags = 0;
+    target_mem =
+        lzt::allocate_shared_memory(mem_size, 1, d_flags, h_flags, dev1);
+  }
+
+  // on dev1, load image to dev1 memory
+  lzt::append_memory_copy(command_list_dev1, target_mem, input_png.raw_data(),
+                          mem_size, event1);
+  lzt::append_wait_on_events(command_list_dev1, 1, &event1);
+
+  // on dev1, copy from dev1 memory to dev0 image
+  lzt::append_image_copy_from_mem(command_list_dev1, img_dev0, target_mem,
+                                  event2);
+  lzt::append_wait_on_events(command_list_dev1, 1, &event2);
+  lzt::close_command_list(command_list_dev1);
+  lzt::execute_command_lists(command_q_dev1, 1, &command_list_dev1, nullptr);
+  lzt::synchronize(command_q_dev1, UINT64_MAX);
+
+  // Copyback to host
+  lzt::append_image_copy_to_mem(command_list_dev0, output_png.raw_data(),
+                                img_dev0, nullptr);
+  lzt::close_command_list(command_list_dev0);
+  lzt::execute_command_lists(command_q_dev0, 1, &command_list_dev0, nullptr);
+  lzt::synchronize(command_q_dev0, UINT64_MAX);
+
+  // compare results
+  EXPECT_EQ(input_png, output_png);
+
+  lzt::destroy_event(event1);
+  lzt::destroy_event(event2);
+  lzt::free_memory(target_mem);
+}
+
 INSTANTIATE_TEST_CASE_P(P2PImageMemory, P2PImageCopyMemory,
                         testing::Values(ZE_MEMORY_TYPE_DEVICE,
                                         ZE_MEMORY_TYPE_SHARED));
