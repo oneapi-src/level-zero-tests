29720b13f87ab7483f5034be0fef296c3b278542
Bari, Pratik <pratik.bari@intel.com>
2024-05-29 05:15:30 +0000

    Changes in Memory test for Out of Device Memory Check
    The test allocates max allocatable memory size at the start and then
    allocates device memory and invokes kernel in multiple iterations
    until the device memory is exhausted
    
    Related-To: NEO-11371
    
    Signed-off-by: Bari, Pratik <pratik.bari@intel.com>

--- conformance_tests/sysman/test_sysman_memory/src/test_sysman_memory.cpp
+++ conformance_tests/sysman/test_sysman_memory/src/test_sysman_memory.cpp
@@ -251,14 +251,72 @@ ze_device_handle_t get_core_device_by_uuid(uint8_t *uuid) {
 }
 #endif // USE_ZESINIT
 
+ze_result_t copy_workload(ze_device_handle_t device,
+                          ze_device_mem_alloc_desc_t *device_desc,
+                          void *src_ptr, void *dst_ptr, int32_t local_size) {
+
+  ze_result_t result = ZE_RESULT_SUCCESS;
+  void *src_buffer = src_ptr;
+  void *dst_buffer = dst_ptr;
+  int32_t offset = 0;
+
+  ze_module_handle_t module = lzt::create_module(
+      device, "copy_module.spv", ZE_MODULE_FORMAT_IL_SPIRV, nullptr, nullptr);
+  ze_kernel_handle_t function = lzt::create_function(module, "copy_data");
+
+  lzt::set_group_size(function, 1, 1, 1);
+  lzt::set_argument_value(function, 0, sizeof(src_buffer), &src_buffer);
+  lzt::set_argument_value(function, 1, sizeof(dst_buffer), &dst_buffer);
+  lzt::set_argument_value(function, 2, sizeof(int32_t), &offset);
+  lzt::set_argument_value(function, 3, sizeof(int32_t), &local_size);
+
+  ze_command_list_handle_t cmd_list = lzt::create_command_list(device);
+
+  const int32_t group_count_x = 1;
+  const int32_t group_count_y = 1;
+  ze_group_count_t tg;
+  tg.groupCountX = group_count_x;
+  tg.groupCountY = group_count_y;
+  tg.groupCountZ = 1;
+
+  result = zeCommandListAppendLaunchKernel(cmd_list, function, &tg, nullptr, 0,
+                                           nullptr);
+  if (result != ZE_RESULT_SUCCESS) {
+    return result;
+  }
+
+  lzt::append_barrier(cmd_list, nullptr, 0, nullptr);
+  lzt::close_command_list(cmd_list);
+  ze_command_queue_handle_t cmd_q = lzt::create_command_queue(device);
+
+  result = zeCommandQueueExecuteCommandLists(cmd_q, 1, &cmd_list, nullptr);
+  if (result != ZE_RESULT_SUCCESS) {
+    return result;
+  }
+
+  lzt::synchronize(cmd_q, UINT64_MAX);
+  lzt::destroy_command_queue(cmd_q);
+  lzt::destroy_command_list(cmd_list);
+  lzt::destroy_function(function);
+  lzt::destroy_module(module);
+
+  return result;
+}
+
 TEST_F(
     MEMORY_TEST,
     GivenValidMemHandleWhenAllocatingMemoryUptoMaxCapacityThenOutOfDeviceMemoryErrorIsReturned) {
 
   for (ze_device_handle_t device : devices) {
-    uint32_t count = 0;
-    uint64_t total_free = 0;
-    std::vector<void *> vec_ptr;
+    ze_device_mem_alloc_desc_t device_desc = {};
+    device_desc.stype = ZE_STRUCTURE_TYPE_DEVICE_MEM_ALLOC_DESC;
+    device_desc.ordinal = 0;
+    device_desc.flags = ZE_DEVICE_MEM_ALLOC_FLAG_BIAS_UNCACHED;
+    device_desc.pNext = nullptr;
+
+    std::vector<void *> alloc_list;
+    ze_result_t result = ZE_RESULT_SUCCESS;
+
 #ifdef USE_ZESINIT
     auto sysman_device_properties = lzt::get_sysman_device_properties(device);
     ze_device_handle_t core_device =
@@ -266,16 +324,20 @@ TEST_F(
     EXPECT_NE(core_device, nullptr);
     ze_device_properties_t deviceProperties = {
         ZE_STRUCTURE_TYPE_DEVICE_PROPERTIES, nullptr};
-    zeDeviceGetProperties(core_device, &deviceProperties);
+    result = zeDeviceGetProperties(core_device, &deviceProperties);
+    EXPECT_EQ(ZE_RESULT_SUCCESS, result);
     std::cout << "test device name " << deviceProperties.name << " uuid "
               << lzt::to_string(deviceProperties.uuid);
+    device = core_device;
 #else  // USE_ZESINIT
     ze_device_properties_t deviceProperties = {
         ZE_STRUCTURE_TYPE_DEVICE_PROPERTIES, nullptr};
-    zeDeviceGetProperties(device, &deviceProperties);
+    result = zeDeviceGetProperties(device, &deviceProperties);
+    EXPECT_EQ(ZE_RESULT_SUCCESS, result);
     std::cout << "test device name " << deviceProperties.name << " uuid "
               << lzt::to_string(deviceProperties.uuid);
 #endif // USE_ZESINIT
+
     if (deviceProperties.flags & ZE_DEVICE_PROPERTY_FLAG_SUBDEVICE) {
       std::cout << "test subdevice id " << deviceProperties.subdeviceId;
     } else {
@@ -283,53 +345,54 @@ TEST_F(
     }
 
     uint64_t max_alloc_size = deviceProperties.maxMemAllocSize;
-    uint64_t alloc_size = ((uint64_t)4 * 1024 * 1024 * 1024);
-    if (max_alloc_size < alloc_size) {
-      alloc_size = max_alloc_size;
-    }
-    uint64_t free_memory = get_free_memory_state(device);
-    void *ze_buf = nullptr;
-    ze_result_t result = ZE_RESULT_SUCCESS;
-    uint8_t pattern = 0xAB;
+    uint64_t alloc_size = max_alloc_size;
+    void *local_mem = nullptr;
+    result = zeMemAllocDevice(lzt::get_default_context(), &device_desc,
+                              alloc_size, 1, device, &local_mem);
+    EXPECT_EQ(ZE_RESULT_SUCCESS, result);
 
-    auto local_mem = lzt::allocate_host_memory(alloc_size);
-    EXPECT_NE(nullptr, local_mem);
-    std::memset(local_mem, pattern, alloc_size);
     result = zeContextMakeMemoryResident(lzt::get_default_context(), device,
                                          local_mem, alloc_size);
-    EXPECT_EQ(result, ZE_RESULT_SUCCESS);
+    EXPECT_EQ(ZE_RESULT_SUCCESS, result);
 
-    uint64_t cur_mem_alloc_size = 0;
+    result = copy_workload(device, &device_desc, local_mem,
+                           static_cast<int *>(local_mem) + alloc_size / 2,
+                           alloc_size / 2);
+    EXPECT_EQ(ZE_RESULT_SUCCESS, result);
+
+    result = zeMemFree(lzt::get_default_context(), local_mem);
+    EXPECT_EQ(ZE_RESULT_SUCCESS, result);
+
+    uint32_t count = 0;
     do {
-      ze_buf = nullptr;
-      if (alloc_size <= free_memory) {
-        cur_mem_alloc_size = alloc_size;
-      } else {
-        cur_mem_alloc_size = free_memory;
-      }
-      auto ze_buf = lzt::allocate_device_memory(cur_mem_alloc_size);
-      EXPECT_NE(nullptr, ze_buf);
-      if (ze_buf != nullptr) {
-        vec_ptr.push_back(ze_buf);
-      } else {
-        std::cout << "Memory Allocation Failed..." << std::endl;
+      local_mem = nullptr;
+      alloc_size = max_alloc_size;
+      result = zeMemAllocDevice(lzt::get_default_context(), &device_desc,
+                                alloc_size, 1, device, &local_mem);
+      if (result != ZE_RESULT_SUCCESS) {
         break;
       }
 
+      alloc_list.push_back(local_mem);
       result = zeContextMakeMemoryResident(lzt::get_default_context(), device,
-                                           ze_buf, cur_mem_alloc_size);
-      free_memory = get_free_memory_state(device);
-    } while ((result != ZE_RESULT_ERROR_OUT_OF_DEVICE_MEMORY) &&
-             (free_memory > 0));
+                                           local_mem, alloc_size);
+      if (result != ZE_RESULT_SUCCESS) {
+        break;
+      }
+
+      result = copy_workload(device, &device_desc, local_mem,
+                             static_cast<uint8_t *>(local_mem) + alloc_size / 2,
+                             alloc_size / 2);
+      count++;
+    } while (result != ZE_RESULT_SUCCESS || count < 10);
 
     EXPECT_EQ(ZE_RESULT_ERROR_OUT_OF_DEVICE_MEMORY, result);
 
-    // Free allocated device memory
-    for (int i = 0; i < vec_ptr.size(); i++) {
-      zeMemFree(lzt::get_default_context(), vec_ptr[i]);
+    for (int i = 0; i < alloc_list.size(); i++) {
+      result = zeMemFree(lzt::get_default_context(), alloc_list[i]);
+      EXPECT_EQ(ZE_RESULT_SUCCESS, result);
     }
-    vec_ptr.clear();
-    lzt::free_memory(local_mem);
+    alloc_list.clear();
   }
 }
 
