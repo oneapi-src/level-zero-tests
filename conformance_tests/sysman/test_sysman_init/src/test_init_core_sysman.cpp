f7331e14648031859fb60cb4c89416dfcafe791c
Vishnu Khanth <vishnu.khanth.b@intel.com>
2024-08-02 17:23:36 +0530

    refactor : Sysman init CTS
     * refactor sysman init tests into separate executable for isolated
       testing.
    
    Related-To: VLCLJ-2253
    
    Signed-off-by: Vishnu Khanth <vishnu.khanth.b@intel.com>

--- conformance_tests/sysman/test_sysman_init/src/test_init_core_sysman.cpp
+++ conformance_tests/sysman/test_sysman_init/src/test_init_core_sysman.cpp
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright (C) 2023 Intel Corporation
+ * Copyright (C) 2024 Intel Corporation
  *
  * SPDX-License-Identifier: MIT
  *
@@ -11,8 +11,6 @@
 #include "logging/logging.hpp"
 #include "utils/utils.hpp"
 #include "test_harness/test_harness.hpp"
-
-namespace lzt = level_zero_tests;
 
 #include <level_zero/zes_api.h>
 
@@ -39,35 +37,6 @@ TEST(
     char enable_sysman_env[] = "ZES_ENABLE_SYSMAN=1";
     putenv(enable_sysman_env);
   }
-}
-
-TEST(SysmanInitTests, GivenSysmanInitializedThenCallingCoreInitSucceeds) {
-  ASSERT_EQ(ZE_RESULT_SUCCESS, zesInit(0));
-  uint32_t zesInitCount = 0;
-  ASSERT_EQ(ZE_RESULT_SUCCESS, zesDriverGet(&zesInitCount, nullptr));
-  ASSERT_GT(zesInitCount, 0);
-  ASSERT_EQ(ZE_RESULT_SUCCESS, zeInit(0));
-  uint32_t zeInitCount = 0;
-  ASSERT_EQ(ZE_RESULT_SUCCESS, zeDriverGet(&zeInitCount, nullptr));
-  ASSERT_GT(zeInitCount, 0);
-}
-
-TEST(SysmanInitTests,
-     GivenCoreNotInitializedWhenSysmanInitializedThenzesDriverGetWorks) {
-  ASSERT_EQ(ZE_RESULT_SUCCESS, zesInit(0));
-  uint32_t pCount = 0;
-  ASSERT_EQ(ZE_RESULT_SUCCESS, zesDriverGet(&pCount, nullptr));
-  ASSERT_GT(pCount, 0);
-}
-
-TEST(SysmanInitTests,
-     GivenEnableSysmanEnvSetWhenCoreInitializedThenzesDriverGetWorks) {
-  static char sys_env[] = "ZES_ENABLE_SYSMAN=1";
-  putenv(sys_env);
-  ASSERT_EQ(ZE_RESULT_SUCCESS, zeInit(0));
-  uint32_t pCount = 0;
-  ASSERT_EQ(ZE_RESULT_SUCCESS, zesDriverGet(&pCount, nullptr));
-  ASSERT_GT(pCount, 0);
 }
 
 } // namespace
