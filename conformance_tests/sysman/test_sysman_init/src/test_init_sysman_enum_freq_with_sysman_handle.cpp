92223ce118932766a57c99992d79bf4b634c23d7
Shreyas Kunder <shreyas.kunder@intel.com>
2024-11-05 18:40:16 +0530

    Sysman init EnumFreqDomain CTS (#72)
    * Sysman EnumFreq CTS
    
    * seperate binary for init cases
    
    Signed-off-by: Kunder, Shreyas <shreyas.kunder@intel.com>
    
    * minor rename
    
    Signed-off-by: Kunder, Shreyas <shreyas.kunder@intel.com>
    
    * Update level_zero_utils
    
    * minor change
    
    * Sysman EnumFreq CTS
    
    * test: sysman init tests from core and sysman handle
    
    Signed-off-by: Shreyas Kunder <shreyas.kunder@intel.com>
    
    * test: sysman init tests from core and sysman handle
    
    Signed-off-by: Shreyas Kunder <shreyas.kunder@intel.com>
    
    * test: sysman init tests from core and sysman handle
    
    Signed-off-by: Shreyas Kunder <shreyas.kunder@intel.com>
    
    * test: sysman init tests from core and sysman handle
    
    Signed-off-by: Shreyas Kunder <shreyas.kunder@intel.com>
    
    * test: sysman init tests from core and sysman handle
    
    Related-To: VLCLJ-2273
    
    Signed-off-by: Shreyas Kunder <shreyas.kunder@intel.com>
    
    * test: sysman init tests from core and sysman handle
    
    Related-To: VLCLJ-2273
    
    Signed-off-by: Shreyas Kunder <shreyas.kunder@intel.com>
    
    ---------
    
    Signed-off-by: Kunder, Shreyas <shreyas.kunder@intel.com>
    Signed-off-by: Shreyas Kunder <shreyas.kunder@intel.com>

---
+++ conformance_tests/sysman/test_sysman_init/src/test_init_sysman_enum_freq_with_sysman_handle.cpp
@@ -0,0 +1,46 @@
+/*
+ *
+ * Copyright (C) 2024 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#include "gtest/gtest.h"
+
+#include "logging/logging.hpp"
+#include "utils/utils.hpp"
+#include "test_harness/test_harness.hpp"
+
+#include <level_zero/zes_api.h>
+
+namespace {
+
+TEST(
+    SysmanInitTests,
+    GivenSysmanInitializationDoneUsingZesInitFollowedByZeInitAlongWithSysmanFlagEnabledThenWhenSysmanApiZesDeviceEnumFrequencyDomainsIsCalledWithSysmanHandleThenSuccessIsReturned) {
+  static char sys_env[] = "ZES_ENABLE_SYSMAN=1";
+  putenv(sys_env);
+
+  ASSERT_EQ(ZE_RESULT_SUCCESS, zesInit(0));
+  ASSERT_EQ(ZE_RESULT_SUCCESS, zeInit(0));
+
+  uint32_t driver_count = 0;
+  ASSERT_EQ(ZE_RESULT_SUCCESS, zesDriverGet(&driver_count, nullptr));
+  ASSERT_GT(driver_count, 0);
+  std::vector<zes_driver_handle_t> drivers(driver_count);
+  ASSERT_EQ(ZE_RESULT_SUCCESS, zesDriverGet(&driver_count, drivers.data()));
+
+  uint32_t device_count = 0;
+  ASSERT_EQ(ZE_RESULT_SUCCESS,
+            zesDeviceGet(drivers[0], &device_count, nullptr));
+  std::vector<zes_device_handle_t> devices(device_count);
+  ASSERT_EQ(ZE_RESULT_SUCCESS,
+            zesDeviceGet(drivers[0], &device_count, devices.data()));
+
+  uint32_t count = 0;
+  EXPECT_EQ(ZE_RESULT_SUCCESS,
+            zesDeviceEnumFrequencyDomains(devices[0], &count, nullptr));
+}
+
+} // namespace
