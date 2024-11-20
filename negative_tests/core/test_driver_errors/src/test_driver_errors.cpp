9fc1b260d58cab9441f5c0b0b472ffd67a365b3d
Neil R. Spruit <neil.r.spruit@intel.com>
2024-11-20 12:26:07 -0800

    Add Tests for zeInitDrivers (#110)
    Signed-off-by: Neil R. Spruit <neil.r.spruit@intel.com>

--- negative_tests/core/test_driver_errors/src/test_driver_errors.cpp
+++ negative_tests/core/test_driver_errors/src/test_driver_errors.cpp
@@ -34,4 +34,46 @@ TEST(
   EXPECT_EQ(ZE_RESULT_ERROR_INVALID_NULL_POINTER,
             zeDriverGet(nullptr, nullptr));
 }
+#ifdef ZE_API_VERSION_CURRENT_M
+TEST(
+  zeInitDriversNegativeTests,
+  GivenCallToZeInitDriversWithNoFlagsThenExpectFailure) {
+
+  uint32_t pCount = 0;
+  ze_init_driver_type_desc_t desc = {ZE_STRUCTURE_TYPE_INIT_DRIVER_TYPE_DESC};
+  desc.pNext = nullptr;
+
+  // Test with no flags
+  desc.flags = 0;
+  EXPECT_EQ(ZE_RESULT_ERROR_INVALID_ENUMERATION, zeInitDrivers(&pCount, nullptr, &desc));
+  EXPECT_EQ(pCount, 0);
+}
+
+TEST(
+  zeInitDriversNegativeTests,
+  GivenCallToZeInitDriversWithNullPointerCountThenExpectFailure) {
+
+  uint32_t pCount = 0;
+  ze_init_driver_type_desc_t desc = {ZE_STRUCTURE_TYPE_INIT_DRIVER_TYPE_DESC};
+  desc.pNext = nullptr;
+
+  // Test with nullptr pCount
+  desc.flags = 0;
+  EXPECT_EQ(ZE_RESULT_ERROR_INVALID_NULL_POINTER, zeInitDrivers(nullptr, nullptr, &desc));
+}
+
+TEST(
+  zeInitDriversNegativeTests,
+  GivenCallToZeInitDriversWithNullPointerDescThenExpectFailure) {
+
+  uint32_t pCount = 0;
+  ze_init_driver_type_desc_t desc = {ZE_STRUCTURE_TYPE_INIT_DRIVER_TYPE_DESC};
+  desc.pNext = nullptr;
+
+  // Test with nullptr Desc
+  EXPECT_EQ(ZE_RESULT_ERROR_INVALID_NULL_POINTER, zeInitDrivers(&pCount, nullptr, nullptr));
+  EXPECT_EQ(pCount, 0);
+}
+#endif
+
 } // namespace
