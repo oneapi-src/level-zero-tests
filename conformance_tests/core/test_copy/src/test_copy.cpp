fa849822ab3cf3b85a5327b8b324ab45a06e6767
John Falkowski <john.falkowski@intel.com>
2025-07-17 21:32:25 +0000

    -Create back-to-back shared system USM to USM tests
    Signed-off-by: John Falkowski <john.falkowski@intel.com>

--- conformance_tests/core/test_copy/src/test_copy.cpp
+++ conformance_tests/core/test_copy/src/test_copy.cpp
@@ -781,15 +781,15 @@ LZT_TEST_F(
   RunGivenDeviceMemoryToDeviceMemoryAndSizeWhenAppendingMemoryCopyTest(true);
 }
 
-class zeCommandListAppendMemoryBackToBackTests 
+class zeCommandListAppendMemoryBackToBackTests
     : public ::testing::Test,
       public ::testing::WithParamInterface<
           std::tuple<bool, bool, bool, bool, size_t>> {
 
 protected:
-    void RunGivenCopyBetweenUsmAndSharedSystemUsmAndVerifyCorrect(
-        bool is_src_shared_system, bool is_dst_shared_system, bool is_immediate,
-        bool isCopyOnly, size_t size) {
+  void RunGivenCopyBetweenUsmAndSharedSystemUsmAndVerifyCorrect(
+      bool is_src_shared_system, bool is_dst_shared_system, bool is_immediate,
+      bool isCopyOnly, size_t size) {
     const uint8_t value = (rand()) & 0xFF;
     const uint8_t init = (value + 0x22) & 0xFF;
 
@@ -803,19 +803,21 @@ protected:
       ordinal = 1;
     }
 
-    const char* src_memory_type = is_src_shared_system ? "SVM" : "USM";
-    const char* dst_memory_type = is_dst_shared_system ? "SVM" : "USM";
+    const char *src_memory_type = is_src_shared_system ? "SVM" : "USM";
+    const char *dst_memory_type = is_dst_shared_system ? "SVM" : "USM";
 
-    printf("src_memory (%s)= %p dst_memory (%s)= %p immediate = %d isCopyOnly = %d size = %ld\n",
-       src_memory_type, src_memory, dst_memory_type, dst_memory, is_immediate, isCopyOnly, size);
+    printf("src_memory (%s)= %p dst_memory (%s)= %p immediate = %d isCopyOnly "
+           "= %d size = %ld\n",
+           src_memory_type, src_memory, dst_memory_type, dst_memory,
+           is_immediate, isCopyOnly, size);
 
     memset(src_memory, value, size);
     memset(dst_memory, init, size);
 
     auto cmd_bundle = lzt::create_command_bundle(
-    lzt::get_default_context(), zeDevice::get_instance()->get_device(), 0, 
-    ZE_COMMAND_QUEUE_MODE_ASYNCHRONOUS, ZE_COMMAND_QUEUE_PRIORITY_NORMAL, 0, 
-    ordinal, 0, is_immediate);
+        lzt::get_default_context(), zeDevice::get_instance()->get_device(), 0, 
+        ZE_COMMAND_QUEUE_MODE_ASYNCHRONOUS, ZE_COMMAND_QUEUE_PRIORITY_NORMAL, 0, 
+        ordinal, 0, is_immediate);
 
     lzt::append_memory_copy(cmd_bundle.list, static_cast<void *>(dst_memory),
                             static_cast<void *>(src_memory), size);
@@ -832,22 +834,18 @@ protected:
 LZT_TEST_P(
     zeCommandListAppendMemoryBackToBackTests,
     GivenUsmAndSharedSystemUsmConfirmSuccessfulExecutionBackToBackAllPermutations) {
-    SKIP_IF_SHARED_SYSTEM_ALLOC_UNSUPPORTED();
-    RunGivenCopyBetweenUsmAndSharedSystemUsmAndVerifyCorrect(std::get<0>(GetParam()),
-                                                             std::get<1>(GetParam()),
-                                                             std::get<2>(GetParam()),
-                                                             std::get<3>(GetParam()),
-                                                             std::get<4>(GetParam()));
-}
+  SKIP_IF_SHARED_SYSTEM_ALLOC_UNSUPPORTED();
+  RunGivenCopyBetweenUsmAndSharedSystemUsmAndVerifyCorrect(
+      std::get<0>(GetParam()), std::get<1>(GetParam()), std::get<2>(GetParam()),
+      std::get<3>(GetParam()), std::get<4>(GetParam()));
+ }
+
 
 INSTANTIATE_TEST_SUITE_P(
     ParameterizedTests, zeCommandListAppendMemoryBackToBackTests,
-    ::testing::Combine(
-        ::testing::Bool(),
-        ::testing::Bool(),
-        ::testing::Bool(),
-        ::testing::Bool(),
-        ::testing::Values(10, 10*1024, 32*1024*1024)));
+    ::testing::Combine(::testing::Bool(), ::testing::Bool(), ::testing::Bool(),
+                       ::testing::Bool(),
+                       ::testing::Values(10, 10 * 1024, 32 * 1024 * 1024)));
 
 
 
