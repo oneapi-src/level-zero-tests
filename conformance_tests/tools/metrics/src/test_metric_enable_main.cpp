d3d1eefd07e4cdaaf0a30c6c9af442cc743166c6
Shreyas Kunder <shreyas.kunder@intel.com>
2025-08-08 17:54:41 +0530

    fix: Disable metrics env variable for metric Enable Disable tests. (#266)
    * fix: Disable metrics env variable for metric Enable Disable tests.
    
    Related to NEO-15708
    
    Signed-off-by: Shreyas Kunder <shreyas.kunder@intel.com>

--- conformance_tests/tools/metrics/src/test_metric_enable_main.cpp
+++ conformance_tests/tools/metrics/src/test_metric_enable_main.cpp
@@ -15,6 +15,17 @@ int main(int argc, char **argv) {
   std::vector<std::string> command_line(argv + 1, argv + argc);
   level_zero_tests::init_logging(command_line);
 
+  const std::string var_enable_metrics = "ZET_ENABLE_METRICS";
+  const char *env_value = std::getenv(var_enable_metrics.c_str());
+  if (env_value != nullptr) {
+    LOG_INFO << "ZET_ENABLE_METRICS=1 is Set. Disabling.";
+#if defined(_WIN32) || defined(_WIN64)
+    _putenv_s(var_enable_metrics.c_str(), "0");
+#else
+    setenv(var_enable_metrics.c_str(), "0", 1) == 0;
+#endif
+  }
+
   ze_result_t result = zeInit(0);
   if (result) {
     throw std::runtime_error("zeInit failed: " +
@@ -23,5 +34,16 @@ int main(int argc, char **argv) {
   LOG_TRACE << "Driver initialized";
 
   LOG_TRACE << "Tools API initialized";
-  return RUN_ALL_TESTS();
+  int return_val = RUN_ALL_TESTS();
+
+  if (env_value != nullptr) {
+    LOG_INFO << "Re-enabling ZET_ENABLE_METRICS=1";
+#if defined(_WIN32) || defined(_WIN64)
+    _putenv_s(var_enable_metrics.c_str(), "1");
+#else
+    setenv(var_enable_metrics.c_str(), "1", 1) == 0;
+#endif
+  }
+
+  return return_val;
 }
