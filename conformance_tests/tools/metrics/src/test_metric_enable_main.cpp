666b345b45c55aa3dc792a2d54f88862bba94b5f
Shreyas Kunder <shreyas.kunder@intel.com>
2025-08-08 12:35:37 +0530

    fix: Disable metrics env variable for metric Enable Disable tests.
    Related to NEO-15708
    
    Signed-off-by: Shreyas Kunder <shreyas.kunder@intel.com>

--- conformance_tests/tools/metrics/src/test_metric_enable_main.cpp
+++ conformance_tests/tools/metrics/src/test_metric_enable_main.cpp
@@ -15,6 +15,13 @@ int main(int argc, char **argv) {
   std::vector<std::string> command_line(argv + 1, argv + argc);
   level_zero_tests::init_logging(command_line);
 
+  const std::string varEnableMetrics = "ZET_ENABLE_METRICS";
+  const char *envValue = std::getenv(varEnableMetrics.c_str());
+  if (envValue != nullptr) {
+    LOG_INFO << "ZET_ENABLE_METRICS=1 is Set. Disabling.";
+    setenv(varEnableMetrics.c_str(), "0", 1) == 0;
+  }
+
   ze_result_t result = zeInit(0);
   if (result) {
     throw std::runtime_error("zeInit failed: " +
@@ -23,5 +30,11 @@ int main(int argc, char **argv) {
   LOG_TRACE << "Driver initialized";
 
   LOG_TRACE << "Tools API initialized";
-  return RUN_ALL_TESTS();
+  int returnVal = RUN_ALL_TESTS();
+
+  if (envValue != nullptr) {
+    LOG_INFO << "Re-enabling ZET_ENABLE_METRICS=1";
+    setenv(varEnableMetrics.c_str(), "1", 1) == 0;
+  }
+  return returnVal;
 }
