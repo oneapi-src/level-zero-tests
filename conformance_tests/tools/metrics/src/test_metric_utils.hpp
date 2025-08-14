56f63384a0e9d45602669eff344a95b13de1b0c1
Narendra Bagria <narendra.bagria@intel.com>
2025-08-14 17:11:11 +0530

    fix: remove metric ip test (#274)
    Related-To: VLCLJ-2551
    
    Signed-off-by: Narendra Bagria <narendra.bagria@intel.com>

--- conformance_tests/tools/metrics/src/test_metric_utils.hpp
+++ conformance_tests/tools/metrics/src/test_metric_utils.hpp
@@ -15,4 +15,14 @@ ze_kernel_handle_t get_matrix_multiplication_kernel(
     ze_device_handle_t device, ze_group_count_t *tg, void **a_buffer,
     void **b_buffer, void **c_buffer, int dimensions = 1024);
 
+void metric_validate_stall_sampling_data(
+    std::vector<zet_metric_properties_t> &metricProperties,
+    std::vector<zet_typed_value_t> &totalMetricValues,
+    std::vector<uint32_t> &metricValueSets);
+
+void metric_run_ip_sampling_with_validation(
+    bool enableOverflow, const std::vector<ze_device_handle_t> &devices,
+    uint32_t notifyEveryNReports, uint32_t samplingPeriod,
+    uint32_t timeForNReportsComplete);
+
 #endif // TEST_METRIC_UTILS_HPP
